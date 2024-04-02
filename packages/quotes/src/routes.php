f00f758d583c7515265b05b6ba71ee6346be1a7c
jesusado <jesusado.89@gmail.com>
2024-04-02 08:31:22 -0400

    correction4

--- packages/quotes/src/routes.php
+++ packages/quotes/src/routes.php
@@ -4,19 +4,16 @@ use Quotes\Http\Controllers\QuotesController;
 
 Route::get('/quotes', [QuotesController::class, 'index']);
 
-Route::middleware('throttle:30,1')->group(function () {
-    Route::get('/quotes', [QuotesController::class, 'index']);
-});
+Route::middleware('throttle:30,1')->group(
+    function () {
+        Route::get('/quotes/random/{number}', [QuotesController::class, 'random']);
+    }
+);
 
-Route::get('/quotes/random/{number}', [QuotesController::class, 'random']);
-Route::get('/quotes/favorites', [QuotesController::class, 'favorites'])->middleware('auth');
-Route::delete('/quotes/favorites/{quote}', [QuotesController::class, 'removeFavorite'])->middleware('auth');
-
-// Agregar las nuevas rutas para manejar las citas favoritas
-
-Route::middleware('auth')->group(function () {
-    Route::post('/quotes/favorites/add', [QuotesController::class, 'addFavorite']);
-    Route::get('/quotes/favorites', [QuotesController::class, 'favorites']);
-    Route::delete('/quotes/favorites/{quote}', [QuotesController::class, 'removeFavorite']);
-});
-
+Route::middleware('auth')->group(
+    function () {
+        Route::get('/quotes/favorites', [QuotesController::class, 'favorites']);
+        Route::delete('/quotes/favorites/{quote}', [QuotesController::class, 'removeFavorite']);
+        Route::post('/quotes/favorites/add', [QuotesController::class, 'addFavorite']);
+    }
+);
