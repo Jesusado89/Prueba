6c137fd1e8c0becad7beb82fd1bb6d7d5de9ab32
jesusado <jesusado.89@gmail.com>
2024-04-02 21:11:22 -0400

    style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,17 @@
 <form method="POST" action="{{ route('profile.update') }}">
-    @csrf
-    <label for="name">Name</label>
-    <input id="name" type="text" name="name" value="{{ old('name', $user->name) }}" required autofocus>
-    <label for="email">Email</label>
-    <input id="email" type="email" name="email" value="{{ old('email', $user->email) }}" required>
-    <button type="submit">Update Profile</button>
+    <div class="mb-3">
+        @csrf
+        <label for="name">Name</label>
+        <input id="name" type="text" name="name" value="{{ old('name', $user->name) }}" required autofocus>
+    </div>
+    <div class="mb-3">
+        <label for="email">Email</label>
+        <input id="email" type="email" name="email" value="{{ old('email', $user->email) }}" required>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Update</button>
 </form>
+
